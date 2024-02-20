45a85044045ebe1087143d2931bb8a2547984f4e
anthonysioux91@outlook.com <anthonysu91@gmail.com>
2024-02-20 15:13:54 +0800

    button added

--- app/src/main/java/com/sioux/anthony/androidapp/homepage/PowerUp.kt
+++ app/src/main/java/com/sioux/anthony/androidapp/homepage/PowerUp.kt
@@ -3,28 +3,21 @@ package com.sioux.anthony.androidapp.homepage
 import android.util.Log
 import android.view.Gravity
 import android.widget.Toast
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.size
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+
 import androidx.compose.material3.Button
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.foundation.text.ClickableText
+
 
 @Composable
 fun PowerUp(navController: NavHostController) {
@@ -35,12 +28,10 @@ fun PowerUp(navController: NavHostController) {
 fun PowerUpView(){
     val context = LocalContext.current
 
-
-    Column(
+    Box(
         modifier = Modifier
-            .fillMaxSize()
-            .align(Alignment.CenterHorizontally) // 水平居中
-            .align(Alignment.CenterVertically) // 垂直居中
+            .fillMaxSize() // 让Box占据全屏
+            .wrapContentSize(Alignment.Center) // 让内容居中
     ){
         Button(onClick = {
             Log.i("button", "一键开启所有设备按钮被点击")
--- app/src/main/java/com/sioux/anthony/androidapp/homepage/ShutDown.kt
+++ app/src/main/java/com/sioux/anthony/androidapp/homepage/ShutDown.kt
@@ -1,7 +1,18 @@
 package com.sioux.anthony.androidapp.homepage
 
 
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 
 @Composable
@@ -11,5 +22,21 @@ fun ShutDown(navController: NavHostController) {
 
 @Composable
 fun ShutDownView(){
+
+    val context = LocalContext.current
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize() // 让Box占据全屏
+            .wrapContentSize(Alignment.Center) // 让内容居中
+    ){
+        Button(onClick = {
+            Log.i("button", "一键关闭所有设备按钮被点击")
+
+            Toast.makeText(context,"一键关闭所有设备按钮被点击", Toast.LENGTH_SHORT).show()
+        }) { Text(text = "一键关闭所有设备",fontSize = 40.sp) }
+
+    }
+
 
 }
